c29bbd0c1ce7d576f55566fdab470484fcd212e2
Murat Can Pehlivan <94754225+Muratcaan@users.noreply.github.com>
2022-05-12 15:45:17 +0300

    finish

---
+++ js/config.js
@@ -0,0 +1,21 @@
+/******************************************************************************************************************/
+/* DEVIN | Responsive Personal Portfolio Theme                                                                    */
+/* (c) Rene Puschinger | https://themeforest.net/user/themegrids                                                  */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["Deneyimli web geliştiricisi <br> ve web tasarımcısıyım."];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Enable the Particles effect */
+var config_enableParticles = true;
+
+/* Google map parameters */
+var config_mapLat = 51.509036; // latitude
+var config_mapLng = -0.123280; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My company'
